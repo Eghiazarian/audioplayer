d1ba400a6c672e6205bf44d94f0f5ab52f8eef78
Armen <eghiazarian.armen@gmail.com>
2024-02-23 13:49:03 +0400

    improved functionality

--- audio.js
+++ audio.js
@@ -1,58 +1,46 @@
-let audios = document.getElementsByTagName('audio')
-let videos = document.getElementsByClassName('video')
+let mediaPlayers = document.getElementsByClassName('mediaPlayer')
 
 
 // for (const audio of audios) {
-    // audio.onplay = (event) => {
-        // pauseOthers(event.target)
-    // }
+// audio.onplay = (event) => {
+// pauseOthers(event.target)
+// }
 // }
 // function pauseOthers(currentAudio) {
-    // for (let audioTag of audios) {
-        // if (currentAudio !== audioTag) {
-            // audioTag.pause()
-            // audioTag.currentTime = 0
-        // }
-    // }
+// for (let audioTag of audios) {
+// if (currentAudio !== audioTag) {
+// audioTag.pause()
+// audioTag.currentTime = 0
+// }
+// }
 // }
 // 
 // for (const video of videos) {
-    // video.onplay = (event) => {
-        // pauseOthersVideo(event.target)
+// video.onplay = (event) => {
+// pauseOthersVideo(event.target)
 // 
-    // }
+// }
 // }
 // function pauseOthersVideo(currentVideo) {
-    // for (let videoTag of videos) {
-        // if (currentVideo !== videoTag) {
-            // videoTag.pause()
-            // videoTag.currentTime = 0
-        // }
-    // }
+// for (let videoTag of videos) {
+// if (currentVideo !== videoTag) {
+// videoTag.pause()
+// videoTag.currentTime = 0
+// }
+// }
 // }
 
-for (let video of videos) {
-    video.onplay = (event) => {
+for (let mediaPlayer of mediaPlayers) {
+    mediaPlayer.onplay = (event) => {
         pauseOthers(event.target)
-
-    }
-    for (let audio of audios) {
-        audio.onplay = (event) => {
-            pauseOthers(event.target)
-
-        }
     }
 }
-function pauseOthers(currentVideoAudio) {
+function pauseOthers(currentMediaPlayer) {
 
-    for (const videoTag of videos) {
-        for (let audioTag of audios) {
-            if (currentVideoAudio !== videoTag && currentVideoAudio !== audioTag) {
-                videoTag.pause()
-                audioTag.pause()
-                videoTag.currentTime = 0
-                audioTag.currentTime = 0
-            }
+    for (let mediaPlayer of mediaPlayers) {
+        if (mediaPlayer !== currentMediaPlayer) {
+            mediaPlayer.pause()
+            mediaPlayer.currentTime = 0
         }
     }
 }
